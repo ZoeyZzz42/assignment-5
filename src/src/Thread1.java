42fa62f4b275d61b496c2b208d51ef851e31c563
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-12-04 12:43:35 -0500

    fix file path error

--- src/src/Thread1.java
+++ src/src/Thread1.java
@@ -1,7 +1,7 @@
 public class Thread1 extends Thread {
     public void run() {
         FilePlayer fp = new FilePlayer();
-        String[] paths = {"do.wav", "mi.wav", "sol.wav", "si.wav", "do-octave.wav"};
+        String[] paths = {"src/src/do.wav", "src/src/mi.wav", "src/src/sol.wav", "src/src/si.wav", "src/src/do-octave.wav"};
         for (String path : paths) {
             fp.play(path);
         }
--- src/src/Thread2.java
+++ src/src/Thread2.java
@@ -1,7 +1,7 @@
 public class Thread2 extends Thread {
     public void run() {
         FilePlayer fp = new FilePlayer();
-        String[] paths = {"re.wav", "fa.wav", "la.wav", "do-octave.wav"};
+        String[] paths = {"src/src/re.wav", "src/src/fa.wav", "src/src/la.wav", "src/src/do-octave.wav"};
         for (String path : paths) {
             fp.play(path);
         }
