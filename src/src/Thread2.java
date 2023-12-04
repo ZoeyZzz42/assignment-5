c43bb3cd4773138e9d649e9aeb0e10121b743725
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-12-04 12:38:21 -0500

    run two threads in main method

--- src/src/Thread2.java
+++ src/src/Thread2.java
@@ -1,4 +1,4 @@
-public class Thread2 {
+public class Thread2 extends Thread {
     public void run() {
         FilePlayer fp = new FilePlayer();
         String[] paths = {"re.wav", "fa.wav", "la.wav", "do-octave.wav"};
